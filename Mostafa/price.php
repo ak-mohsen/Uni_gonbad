31b8cdee7e89d0bdd26c53ca767c4afc907f4b53
Mostafasheikhs <mostafa.sheikh.sangtajan@gmail.com>
2022-06-10 17:06:49 +0430

    PHP for hotel project

---
+++ Mostafa/price.php
@@ -0,0 +1,165 @@
+<html>
+<head>
+<title>هتل بزرگ ايرانيان</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td background="images/index_02.gif"><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td width="38%"><img src="images/index_01.gif" width="380" height="166" alt=""></td>
+        <td width="62%"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+          <tr>
+            <td width="47%" class="white-text"></td>
+            <td width="53%"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+              <tr>
+                <td width="7%"><img src="images/index_05.jpg" width="33" height="32" alt=""></td>
+                 <td width="93%" class="white-text">Tel. +98 911 111 1111<br>
+                  info@iranianhotel.com</td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td height="231" background="images/index_08.jpg"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+      <tr>
+        <td width="6%">&nbsp;</td>
+        <td width="94%"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+          <tr>
+            <td><h2 class="white-text"><strong></strong></h2></td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="10" cellpadding="0">
+              <tr>
+                <td width="3%"><img src="images/Arrow.gif" width="20" height="20"></td>
+                <td width="97%" class="white-text">بالاترين امکانات</td>
+              </tr>
+              <tr>
+                <td><img src="images/Arrow.gif" width="20" height="20"></td>
+                <td class="white-text">فضايي دلنشين و آرام</td>
+              </tr>
+              <tr>
+                <td><img src="images/Arrow.gif" width="20" height="20"></td>
+                <td class="white-text">سفري به ياد ماندني</td>
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><table width="100%" border="0" cellspacing="0" cellpadding="0">
+      <tr>
+        <td><a href="about.php"><img src="images/index_09.gif" alt="" width="197" height="41" border="0"></a></td>
+        <td><a href="reserv.php"><img src="images/index_11.gif" alt="" width="129" height="41" border="0"></a></td>
+        <td><a href="contact.php"><img src="images/index_12.gif" alt="" width="129" height="41" border="0"></a></td>
+        <td><a href="admin/index.php"><img src="images/index_10.gif" alt="" width="131" height="41" border="0"></a></td>
+     <td><a href="p.php"><img src="images/index_13.gif" alt="" width="131" height="41" border="0"></a></td>
+       <td><img src="images/index_13.jpg" width="283" height="41" alt=""></td>
+      </tr>
+    </table></td>
+  </tr>
+  <tr>
+    <td><img src="images/index_14.jpg" width="1000" height="44" alt=""></td>
+  </tr>
+</table>
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="10" cellpadding="0">
+      <tr>
+        <td width="29%" align="center" valign="top"><table width="211" border="0" cellspacing="0" cellpadding="0">
+          <tr>
+            <td><a href="price.php"><img src="images/index_20.gif" alt="" width="211" height="30" border="0"></a></td>
+          </tr>
+          <tr>
+            <td><a href="caffe.php"><img src="images/index_23.gif" alt="" width="211" height="39" border="0"></a></td>
+          </tr>
+          <tr>
+            <td><a href="res.php"><img src="images/index_26.gif" alt="" width="211" height="39" border="0"></a></td>
+          </tr>
+          <tr>
+            <td><a href="fun.php"><img src="images/index_27.gif" alt="" width="211" height="39" border="0"></a></td>
+          </tr>
+          <tr>
+            <td><a href="shop.php"><img src="images/index_28.gif" alt="" width="211" height="39" border="0"></a></td>
+          </tr>
+          </table></td>
+        <td width="71%" valign="top"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+          <tr>
+            <td><img src="images/index_17.gif" width="299" height="26" alt=""></td>
+          </tr>
+          <tr>
+            <td bgcolor="#BED780"><img src="images/spacer.gif" width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td>
+<TABLE style="font-size:8pt" color=#000000 face="Tahoma" bgcolor=#FFCC00 border=1 align=center>
+<TD align=right>&nbsp;&nbsp;قيمت&nbsp;&nbsp;</TD>
+<TD align=right>&nbsp;&nbsp;نوع سرويس &nbsp;&nbsp;</TD>
+<?php
+mysql_connect("localhost","root","");
+mysql_select_db("hotel");
+$query1="select * from product";
+$result1=mysql_query($query1);
+while($row1=mysql_fetch_array($result1))
+ {
+
+?>
+<TR>
+<TD align=center>
+<?php echo $row1['price']; ?>
+</TD>
+<TD align=center>
+<?php echo $row1['name']; ?>
+</TD>
+
+<?php } ?>
+</TR>
+</TABLE>
+
+            </td>
+          </tr>
+          <tr>
+            <td><h2></h2></td>
+          </tr>
+          <tr>
+            <td bgcolor="#BED780"><img src="images/spacer.gif" width="1" height="1"></td>
+          </tr>
+          <tr>
+            <td><table width="100%" border="0" cellspacing="10" cellpadding="0">
+              <tr>
+                <td width="19%"><img src="images/index_32.jpg" width="117" height="93" alt=""></td>
+                <td width="19%"><img src="images/index_34.jpg" width="113" height="93" alt=""></td>
+                <td width="22%"><img src="images/index_36.jpg" width="116" height="93" alt=""></td>
+                <td width="40%"><img src="images/index_38.jpg" width="113" height="93" alt=""></td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+
+              </tr>
+            </table></td>
+          </tr>
+        </table></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+<table width="1000" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td height="53" background="images/index_48.gif"><table width="100%" border="0" cellspacing="10" cellpadding="0">
+      <tr>
+        <td width="75%" align="center" class="white-text">Copyright &copy; IranianHotel. All rights reserved.</td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+</body>
+</html>
